5a3a0c84be54d8fd0942b6eec3c9075021cbd4f5
Yuliia Palamar <yuliyapalamar@gmail.com>
2024-05-05 00:03:02 +0300

    admin side done

--- animatchWeb/Controllers/ReviewController.cs
+++ animatchWeb/Controllers/ReviewController.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
+using animatchWeb.ViewModels;
 
 namespace animatchWeb.Controllers
 {
@@ -16,9 +17,28 @@ namespace animatchWeb.Controllers
             _context = context;
         }
 
-        public async Task<List<Review>> GetAllReviews()
+        public async Task<List<ReviewViewModel>> GetAllReviews()
         {
-            var reviewList = await _context.Review.ToListAsync();
+            var reviewList = await _context.Review
+                .Join(_context.UserInfo,
+                    review => review.UserId,
+                    userInfo => userInfo.Id,
+                    (review, userInfo) => new { Review = review, UserInfo = userInfo })
+                .Join(_context.Anime,
+                    reviewUserInfo => reviewUserInfo.Review.AnimeId,
+                    anime => anime.Id,
+                    (reviewUserInfo, anime) => new ReviewViewModel
+                    {
+                        Id = reviewUserInfo.Review.Id,
+                        UserId = reviewUserInfo.Review.UserId,
+                        AnimeId = reviewUserInfo.Review.AnimeId,
+                        Username = reviewUserInfo.UserInfo.Username,
+                        AnimeName = anime.Name,
+                        Text = reviewUserInfo.Review.Text,
+                        Rate = reviewUserInfo.Review.Rate
+                    })
+                .ToListAsync();
+
             return reviewList;
         }
 
@@ -53,6 +73,28 @@ namespace animatchWeb.Controllers
 
             // Перенаправляємо на сторінку з деталями аніме
             return RedirectToAction("Details", "Home", new { id = animeId });
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> DeleteReview(int reviewId)
+        {
+            // Знаходимо відгук за його Id
+            var review = await _context.Review.FindAsync(reviewId);
+
+            // Перевіряємо, чи відгук існує
+            if (review == null)
+            {
+                return NotFound(); // Якщо відгук не знайдено, повертаємо помилку 404 Not Found
+            }
+
+            // Видаляємо відгук з контексту
+            _context.Review.Remove(review);
+
+            // Зберігаємо зміни в базі даних
+            await _context.SaveChangesAsync();
+
+            // Перенаправляємо на ту ж сторінку (або на іншу, в залежності від вашої логіки)
+            return RedirectToAction("ContentManagement", "Home");
         }
     }
 }
--- animatchWeb/Models/UserInfo.cs
+++ animatchWeb/Models/UserInfo.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿ using Microsoft.AspNetCore.Identity;
 using System.ComponentModel.DataAnnotations;
 
 namespace animatchWeb.Models
